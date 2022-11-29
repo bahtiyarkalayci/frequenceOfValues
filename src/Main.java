d89e82ebebf6b663fda46179732e1eb1ec53c52f
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-29 14:09:06 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int[] arr={10,20,20,10,10,20,5,20};
+       List<Integer> frequency=new ArrayList<>();
+       int counter=0;
+        System.out.println("array:"+Arrays.toString(arr));
+        System.out.println("tekrar sayıları:");
+
+       for (int i=0;i<arr.length;i++){
+           for (int j=0;j<arr.length;j++){
+               if ( arr[i]==arr[j] ){
+                    counter++;
+
+
+               }
+
+           }
+           if (!(frequency.contains(arr[i]))&& counter>0){
+               frequency.add(arr[i]);
+               System.out.println(arr[i]+" sayısı "+counter+"  kere tekrar edildi");
+           }
+            counter=0;
+
+       }
+
+
+
     }
 }
